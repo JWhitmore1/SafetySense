0c56d3a850ef01a5341a52427eb3b4c209372f65
JWhitmore1 <38652089+JWhitmore1@users.noreply.github.com>
2024-08-21 16:52:27 +1000

    Remove default screen and build ios

---
+++ src/screens/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>Safety Sense App</Text>
+    </View>
+  );
+}
+
+export default App;
