70edf9a2f017ff0cc895971d7f4ebbbc905e152b
JWhitmore1 <38652089+JWhitmore1@users.noreply.github.com>
2024-10-11 14:12:15 +1000

    Remove Homepage testing

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import {Text} from 'react-native';
 import Navigator from './src/navigation/Navigator';
-import HomeScreen from './src/screens/Homepage';
 
 const App = () => {
   return (
     <>
-      <HomeScreen />
+      <Navigator />
     </>
   );
 };
