b8f0183112788bd4271120e39f016046279027d0
JWhitmore1 <38652089+JWhitmore1@users.noreply.github.com>
2024-10-20 18:33:25 +1000

    Adjust notification and app logo

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import Navigator from './src/navigation/Navigator';
 import { useBleServer } from './src/hooks/useBleServer';
 import ConnectingScreen from './src/screens/ConnectingScreen';
+import { useEffect } from 'react';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 const App = () => {
   const dataAvailable = useBleServer(false);
+
+  useEffect(() => {
+    PushNotificationIOS.requestPermissions();
+  }, [])
 
   return dataAvailable ? (
     <Navigator />
--- src/utils/notify.ts
+++ src/utils/notify.ts
@@ -1,14 +1,12 @@
 import PushNotificationIOS, { NotificationRequest } from "@react-native-community/push-notification-ios";
 import { dataCategory } from "../data/ServerData";
+import icons from "../data/icons";
 
 export const notify = (category: dataCategory) => {
   const request: NotificationRequest = {
     id: `${category}-warning`,
     title: `${category} warning`,
-    subtitle: `SafetySense has detected a ${category} over its threshold!`,
-    isCritical: true,
-    criticalSoundVolume: 1,
+    body: `SafetySense has detected ${category.toLowerCase()} at an unsafe level!`,
   }
-  console.log('sending notification');
   PushNotificationIOS.addNotificationRequest(request);
 } 
